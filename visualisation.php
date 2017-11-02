05f3012fb49cc797a94cc0c34e5df096381cad67
rhum1s <romain.souweine@hotmail.fr>
2017-11-02 14:12:05 +0100

    Nettoyage code

--- visualisation.php
+++ visualisation.php
@@ -525,8 +525,7 @@ function getBase64Image(img) {
     ctx.drawImage(img, 0, 0);
 
     var dataURL = canvas.toDataURL("image/jpeg");
-    
-    console.log("Returning");
+
     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 
 };
@@ -2578,15 +2577,6 @@ function ajouter_logo(){
     };
     logo.addTo(map);  
 };
-
-// function sleep(milliseconds) {
-    // var start = new Date().getTime();
-    // for (var i = 0; i < 1e7; i++) {
-        // if ((new Date().getTime() - start) > milliseconds){
-            // break;
-        // };
-    // };
-// };
 
 /* Appel des fonctions */
 var map = createMap();
